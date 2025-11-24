9f79f4f46fdda54e6523e6fdf2c37e1cfc52c1dd
ItIsNoTToi <giang@gianghung.com>
2025-11-24 11:52:45 +0700

    dsfs

---
+++ sources/qlvt/getdata.sql
@@ -0,0 +1,1 @@
+SELECT * FROM read_parquet('s3://10/csdlqlvt/evidence_report/sources/qlvt/getdata.parquet')
--- sources/test1/getdata.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM read_parquet('s3://10/csdlqlvt/evidence_report%2Fsources%2Fqlvt%2F/getdata.parquet')
